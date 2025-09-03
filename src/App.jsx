4ac64baca936a45dc0eea8328d64a3ad61b8207e
smriti <smritisharma1980@gmail.com>
2025-09-02 21:17:08 -0400

    Fix BrowserRouter basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-// App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 
 import Nav from "./component/Nav";
 import Home from "./component/Home";
 import Weblist from "./component/Weblist";
-import Footer from "./component/Footer"
+import Footer from "./component/Footer";
 import "./Slide1.css";
 import Fetchdata from "./component/Fetchdata";
 
-
 const App = () => {
   return (
-    <BrowserRouter>
+    // Add basename here
+    <BrowserRouter basename="/web_series">
       <div className="app-container">
         <Nav />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/fetch" element={<Fetchdata></Fetchdata>} />
-            
+            <Route path="/fetch" element={<Fetchdata />} />
             <Route path="/weblist" element={<Weblist />} />
           </Routes>
         </main>
@@ -29,6 +26,5 @@ const App = () => {
     </BrowserRouter>
   );
 };
-
 
 export default App;
